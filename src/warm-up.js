79ddd3e2a7f597cb23e9eb485fc1e19da7662610
David Swatzell <thatsstrikethree@gmail.com>
2023-11-29 15:23:35 -0600

    finished variables section

--- src/warm-up.js
+++ src/warm-up.js
@@ -17,14 +17,19 @@ var inStock = 10
 
 // Reassign (not initialize) your "inStock" variable from above. We got a new shipment! Use a built in math operator to make 
 // the inStock count double what it currently is.
+inStock = inStock * 2
+
 
 
 // Reassign (not initialize) your "inStock" variable from above. You got hungry and needed a snack! 
 // Use a built in math operator to decrement the inStock count by 1.
+inStock = inStock - 1
+console.log(inStock)
 
 
 // Initialize a new variable "isFavorite", and assign it to a boolean value indicating
 // if the food you chose is your favorite food or not
+var isFavorite = true
 
 
 
@@ -32,6 +37,11 @@ var inStock = 10
 // assign a boolean value to this variable. If "inStock" is less than 20, the
 // variable should be true, otherwise it should be false. You MUST use a
 // comparison operator.
+var shouldOrderMore = inStock < 20
+console.log(shouldOrderMore)
+
+
+
 
 
 
