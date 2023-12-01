50a6524d3aa1fcd562bb0ba6ff939d17b5ba731a
David Swatzell <thatsstrikethree@gmail.com>
2023-12-01 16:26:25 -0600

    finished the first 2 problems

--- src/meal.js
+++ src/meal.js
@@ -1,12 +1,29 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
+
+function createMenuItem(name, price, type){
+  var menuItem = {
+    name: name,
+    price: price,
+    type: type
+  }
+  return menuItem
+}
+
+function addIngredients(ingredientName , ingredients){
+  ingredients.push(ingredientName)
+  
+
+return addIngredients
+}
+
 
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
+  createMenuItem,
+  addIngredients,
   // formatPrice,
   // decreasePrice,
   // createRecipe
