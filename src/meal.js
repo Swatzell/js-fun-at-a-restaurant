b2abc1d8b3dd2b0298ebb10f29a0872374af80c6
David Swatzell <thatsstrikethree@gmail.com>
2023-12-02 11:55:42 -0600

    finished meals section

--- src/meal.js
+++ src/meal.js
@@ -11,22 +11,52 @@ function createMenuItem(name, price, type){
   return menuItem
 }
 
-function addIngredients(ingredientName , ingredients){
-  ingredients.push(ingredientName)
+// function addIngredients(ingredientName , ingredients){
+//   for (i=0; i < ingredients.length; i++){
+//     if (ingredients[i] === ingredientName) 
+//   }
+//   ingredients.push(ingredientName)
   
 
-return addIngredients
+ 
+//   return addIngredients
+// }
+
+function addIngredients(ingredientName , ingredientArray){
+  if (ingredientArray.includes(ingredientName) === false){
+      ingredientArray.push(ingredientName)
+  }
+  return ingredientArray
 }
 
 
+function formatPrice(addSymbol){
+  
+  return '$' + addSymbol
+}
+
+function decreasePrice(number){
+  var newPrice = number - (number * .10)  
+  return newPrice
+}
+
+// should return an object
+function createRecipe(recipeTitle, recipeIngredients, recipeType){
+  var recipe = {
+    title: recipeTitle,
+    ingredients: recipeIngredients,
+    type: recipeType,
+  }
+  return recipe
+}
 
 module.exports = {
   nameMenuItem,
   createMenuItem,
   addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
