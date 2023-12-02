0170f15aa67d9858879510095c56b83321c5822a
David Swatzell <thatsstrikethree@gmail.com>
2023-12-02 11:56:50 -0600

    initial order section commit

--- src/order.js
+++ src/order.js
@@ -1,7 +1,34 @@
 
+function takeOrder(orders, deliveryOrders){
+  if (!deliveryOrders.includes(orders) && deliveryOrders.length < 3){
+    deliveryOrders.push(orders);
+  }
+
+}
+
+function refundOrder(orderNumber, deliveryOrders) {
+  for (i = 0; i < deliveryOrders.length; i++) {
+    if (deliveryOrders[i].orderNumber === orderNumber){
+      deliveryOrders.splice(i, 1)
+    }
+  }
+}
+
+function listItems(orderArray){
+  var itemString = ""
+  for (var i = 0; i < orderArray.length; i++){
+    if (itemString === ""){
+      itemString = orderArray[i].item
+    } else {
+      itemString = `${itemString}, ${orderArray[i].item}`
+    } 
+  }
+  return itemString
+}
+
 module.exports = {
-  // takeOrder,
-  // refundOrder,
-  // listItems,
+  takeOrder,
+  refundOrder,
+  listItems,
   // searchOrder
 }
